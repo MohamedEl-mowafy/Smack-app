cc63714abc6fea94469c703e5c4f2923f9559dd6
MohamedEl-mowafy <80730320+MohamedEl-mowafy@users.noreply.github.com>
2021-05-02 08:30:05 +0200

    endLesson4

---
+++ smack/Controller/LoginVC.swift
@@ -0,0 +1,29 @@
+//
+//  LoginVC.swift
+//  smack
+//
+//  Created by Mohamed Elmowafy on 02/05/2021.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    
+    
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closedPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
---
+++ smack/Utilities/Constant.swift
@@ -0,0 +1,12 @@
+//
+//  Constant.swift
+//  smack
+//
+//  Created by Mohamed Elmowafy on 02/05/2021.
+//
+
+import Foundation
+
+// Segues
+
+let TO_LOGIN = "toLogin"
